73fc405e39540be9fc44fcd88160f8566a01b449
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2020-01-16 21:07:02 -0500

    added inputs to create screen

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet} from 'react-native';
 
 const CreateScreen = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
   return (
     <View>
-      <Text>Create Screen</Text>
+      <Text style={styles.label} >Enter Title:</Text>
+      <TextInput value={title} onChangeText={(text) => setTitle(text)} style={styles.input} />
+      <Text style={styles.label}>Enter Content:</Text>
+      <TextInput value={content} onChangeText={(text) => setContent(text)} style={styles.input} />
+      <Button title="Add Blog Post" />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18
+    ,borderWidth: 1
+    ,borderColor: 'black'
+    ,marginBottom: 15
+    ,padding: 5
+    ,margin: 5
+  }
+  ,label: {
+    fontSize: 20
+    ,marginBottom: 5
+    ,marginLeft: 5
+  }
+});
 
 export default CreateScreen;
