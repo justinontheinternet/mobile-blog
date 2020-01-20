cd3e8042d5856f279db8176f878bac06188ebd7e
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2020-01-19 21:14:32 -0500

    edit save now goes back to previous screen

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -11,7 +11,7 @@ const EditScreen = ({ navigation }) => {
   return (
     <BlogPostForm
       initialValues={{ title: blogPost.title, content: blogPost.content }}
-      onSubmit={(title, content) => editBlogPost(id, title, content)}
+      onSubmit={(title, content) => editBlogPost(id, title, content, () => navigation.pop())}
     />
   );
 };
