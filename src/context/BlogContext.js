598ec0983e2d26ae97421afbfd496f0f8ab66cea
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2020-01-23 20:58:39 -0500

    removed add_blogpost reducer case

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -10,15 +10,15 @@ const blogReducer = (state, action) => {
   switch (action.type) {
     case 'get_blogposts':
       return action.payload;
-    case 'add_blogpost':
-      return [
-        ...state
-        ,{
-          id: Math.floor(Math.random() * 99999)
-          ,title: action.payload.title
-          ,content: action.payload.content
-        }
-      ];
+    // case 'add_blogpost':
+    //   return [
+    //     ...state
+    //     ,{
+    //       id: Math.floor(Math.random() * 99999)
+    //       ,title: action.payload.title
+    //       ,content: action.payload.content
+    //     }
+    //   ];
     case 'edit_blogpost':
       return state.map((blogPost) => {
         return blogPost.id === action.payload.id ? action.payload : blogPost;
