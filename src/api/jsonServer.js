1dae56469eb253a2cf6ca68c1075a39f7938dc10
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2020-01-26 13:47:47 -0500

    test commit

--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
   // free ngrok requires this url to change every 8 hours
-  baseURL: 'http://e7f55184.ngrok.io'
+  baseURL: 'http://18974c5c.ngrok.io'
 });
